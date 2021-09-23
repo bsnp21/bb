8bef73f5145522964671aa660806ac37bade7ead
wdingsoft <wdingbox@gmail.com>
2021-09-23 02:48:12 -0400

    split utilib

--- np/BibleStudyNotePad.js
+++ np/BibleStudyNotePad.js
@@ -3716,66 +3716,6 @@ var Uti = {
     },
 
 
-    copyTextToClipboard: function (text) {
-        var textArea = document.createElement("textarea");
-
-        //
-        // *** This styling is an extra step which is likely not required. ***
-        //
-        // Why is it here? To ensure:
-        // 1. the element is able to have focus and selection.
-        // 2. if the element was to flash render it has minimal visual impact.
-        // 3. less flakyness with selection and copying which **might** occur if
-        //    the textarea element is not visible.
-        //
-        // The likelihood is the element won't even render, not even a
-        // flash, so some of these are just precautions. However in
-        // Internet Explorer the element is visible whilst the popup
-        // box asking the user for permission for the web page to
-        // copy to the clipboard.
-        //
-
-        // Place in the top-left corner of screen regardless of scroll position.
-        textArea.style.position = 'fixed';
-        textArea.style.top = 0;
-        textArea.style.left = 0;
-
-        // Ensure it has a small width and height. Setting to 1px / 1em
-        // doesn't work as this gives a negative w/h on some browsers.
-        textArea.style.width = '2em';
-        textArea.style.height = '2em';
-
-        // We don't need padding, reducing the size if it does flash render.
-        textArea.style.padding = 0;
-
-        // Clean up any borders.
-        textArea.style.border = 'none';
-        textArea.style.outline = 'none';
-        textArea.style.boxShadow = 'none';
-
-        // Avoid flash of the white box if rendered for any reason.
-        textArea.style.background = 'transparent';
-
-
-        textArea.value = text;
-
-        document.body.appendChild(textArea);
-        textArea.focus();
-        textArea.select();
-
-        try {
-            var successful = document.execCommand('copy');
-            var msg = successful ? 'successful' : 'unsuccessful';
-            console.log('Copying text command was ' + msg);
-            Uti.Msg('Copying text command was ' + msg)
-        } catch (err) {
-            console.log('Oops, unable to copy');
-            Uti.Msg('Oops, unable to copy')
-        }
-
-        document.body.removeChild(textArea);
-
-    },
     copy2clipboard: function (text, ele) {
         //return this.copyTextToClipboard(text)
         $("#txt_copy2clicpboard").val(text).select().focus();
@@ -3791,22 +3731,6 @@ var Uti = {
         $("#txt_copy2clicpboard").blur()
         $("body").focus()
         return
-        const textarea = document.createElement('textarea')
-
-        if (!ele) {
-            //document.body.appendChild(textarea)
-        } else {
-            //$(ele).append(textarea)
-        }
-        document.body.appendChild(textarea);//.css("position:fixed;top:0;left:0")
-        textarea.style.position = "fixed";
-        textarea.style.top = "0px";
-        textarea.style.left = "0px";
-        //textarea.style.left = "0";
-        textarea.value = text
-        textarea.select()
-        document.execCommand('copy')
-        textarea.remove()
     },
     jq_post: function () {
         $.post(Jsonpster.getUrl(),
