26db49fdfa2531d806033abfabdace6ba27b7c67
wdingsoft <wdingbox@gmail.com>
2021-09-24 13:36:56 -0400

    install node moudles

---
+++ svr/restapi/install_node_module.sh
@@ -0,0 +1,9 @@
+
+
+sudo npm istall express
+sudo npm istall cors
+sudo npm istall cheerio
+sudo npm istall express-fileupload
+sudo npm istall express
+sudo npm istall express
+sudo npm istall express
