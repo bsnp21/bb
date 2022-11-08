278adec0a3a0643d77bf37a5b23d95970084e5bd
wdingsoft <wdingbox@gmail.com>
2022-11-08 16:50:04 -0500

    rename the local var to localvar

--- svr/restapi/MyNodjsModules/BibleObjGituser_mod.js
+++ svr/restapi/MyNodjsModules/BibleObjGituser_mod.js
@@ -542,19 +542,19 @@ var BibleUti = {
         }
         return inp
     },
-    _deplore_usr_proj_dirs: function (usr_proj, base_Dir) {
+    _deplore_usr_proj_dirs: function (userproj, base_Dir) {
         //const base_Dir = "bible_study_notes/usrs"
 
 
-        usr_proj.base_Dir = base_Dir
-        usr_proj.user_dir = `${base_Dir}/${usr_proj.hostname}/${usr_proj.username}`
-        usr_proj.git_root = `${base_Dir}/${usr_proj.hostname}/${usr_proj.username}/${usr_proj.projname}`
-        usr_proj.acct_dir = `${base_Dir}/${usr_proj.hostname}/${usr_proj.username}/${usr_proj.projname}/account`
-        usr_proj.dest_myo = `${base_Dir}/${usr_proj.hostname}/${usr_proj.username}/${usr_proj.projname}/account/myoj`
-        usr_proj.dest_dat = `${base_Dir}/${usr_proj.hostname}/${usr_proj.username}/${usr_proj.projname}/account/dat`
+        userproj.base_Dir = base_Dir
+        userproj.user_dir = `${base_Dir}/${userproj.hostname}/${userproj.username}`
+        userproj.git_root = `${base_Dir}/${userproj.hostname}/${userproj.username}/${userproj.projname}`
+        userproj.acct_dir = `${base_Dir}/${userproj.hostname}/${userproj.username}/${userproj.projname}/account`
+        userproj.dest_myo = `${base_Dir}/${userproj.hostname}/${userproj.username}/${userproj.projname}/account/myoj`
+        userproj.dest_dat = `${base_Dir}/${userproj.hostname}/${userproj.username}/${userproj.projname}/account/dat`
 
 
-        console.log("deplore: usr_proj=", usr_proj)
+        console.log("deplore: userproj=", userproj)
     },
 
     _interpret_repo_url_str: function (proj_url) {
@@ -613,32 +613,32 @@ var BibleUti = {
         return null
     },
 
-    _interpret_git_config_Usr_Pwd_Url: function (usr_proj, passcode) {
-        usr_proj.git_Usr_Pwd_Url = ""
+    _interpret_git_config_Usr_Pwd_Url: function (userproj, passcode) {
+        userproj.git_Usr_Pwd_Url = ""
         if (passcode.trim().length > 0) {
-            if ("github.com" === usr_proj.hostname) {
-                usr_proj.git_Usr_Pwd_Url = `https://${usr_proj.username}:${passcode}@${usr_proj.hostname}/${usr_proj.username}/${usr_proj.projname}.git`
+            if ("github.com" === userproj.hostname) {
+                userproj.git_Usr_Pwd_Url = `https://${userproj.username}:${passcode}@${userproj.hostname}/${userproj.username}/${userproj.projname}.git`
             }
-            if ("bitbucket.org" === usr_proj.hostname) {
-                usr_proj.git_Usr_Pwd_Url = `https://${usr_proj.username}:${passcode}@${usr_proj.hostname}/${usr_proj.prjbitbk}/${usr_proj.projname}.git`
+            if ("bitbucket.org" === userproj.hostname) {
+                userproj.git_Usr_Pwd_Url = `https://${userproj.username}:${passcode}@${userproj.hostname}/${userproj.prjbitbk}/${userproj.projname}.git`
             }
         }
 
         //inp.usr.repodesc = inp.usr.repodesc.trim().replace(/[\r|\n]/g, ",")//:may distroy cmdline.
     },
-    _parse_inp_usr2proj: function (repopath, passcode) {
+    _parse_inp_usr2proj: function (repopath, passcode, m_sBaseUsrs) {
         //this.m_inp = inp
-        var usr_proj = BibleUti._interpret_repo_url_str(repopath)//inp.usr.repopath
+        var userproj = BibleUti._interpret_repo_url_str(repopath)//inp.usr.repopath
 
-        BibleUti._deplore_usr_proj_dirs(usr_proj, this.m_sBaseUsrs)
+        BibleUti._deplore_usr_proj_dirs(userproj, m_sBaseUsrs)
 
         if (passcode.trim().length > 0) {
-            BibleUti._interpret_git_config_Usr_Pwd_Url(usr_proj, passcode)//inp.usr.passcode
+            BibleUti._interpret_git_config_Usr_Pwd_Url(userproj, passcode)//inp.usr.passcode
         }
 
         //BibleUti._parse_inp_usr2proj
-        //this.m_inp.usr_proj = usr_proj
-        return usr_proj
+        //this.m_inp.userproj = userproj
+        return userproj
     },
 
 
