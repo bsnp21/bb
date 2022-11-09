6df108e46ba0b11cce6381136f3c41be90cd83f2
wdingsoft <wdingbox@gmail.com>
2022-11-08 19:55:51 -0500

    very messy

--- svr/restapi/MyNodjsModules/BibleObjGituser_mod.js
+++ svr/restapi/MyNodjsModules/BibleObjGituser_mod.js
@@ -1210,7 +1210,7 @@ BibleObjGituser.prototype.Run_proj_setup = function () {
     if (!fs.existsSync(dir)) {
         this.git_clone() //always sucess even passwd is wrong.
     } else {
-        this.m_UserProjFileSys.git_pull()
+        this.git_pull()
     }
 
     if (!fs.existsSync(dir)) {
