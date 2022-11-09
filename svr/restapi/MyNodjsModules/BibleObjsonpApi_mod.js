68aa82c703f5298adf95e39bfc88970d4ad24a12
wdingsoft <wdingbox@gmail.com>
2022-11-08 21:58:47 -0500

    begin to work on obj load

--- svr/restapi/MyNodjsModules/BibleObjsonpApi_mod.js
+++ svr/restapi/MyNodjsModules/BibleObjsonpApi_mod.js
@@ -542,7 +542,7 @@ var ApiJsonp_BibleObj = {
 
             inp.out.state = userProject.m_UserProjFileSys.Run_proj_setup()
             if (inp.out.state.bEditable === 1) {
-                if (null === userProject.git_push_test()) {
+                if (null === userProject.m_UserProjFileSys.git_push_test()) {
                     //inp.out.state.bEditable =  inp.out.state.bRepositable = 0
                     userProject.m_UserProjFileSys.Run_proj_destroy()
                 } else {
