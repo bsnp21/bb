f1e47d04d1c94907ace60612009a0395bcb82740
wdingsoft <wdingbox@gmail.com>
2022-11-09 14:03:00 -0500

    BaseGitUser_mod.js replacement prepare

--- svr/restapi/MyNodjsModules/BibleObjsonpApi_mod.js
+++ svr/restapi/MyNodjsModules/BibleObjsonpApi_mod.js
@@ -537,6 +537,7 @@ var ApiJsonp_BibleObj = {
         ApiUti.Parse_POST_req_to_inp(req, res, function (inp) {
             //: unlimited write size. 
             var userProject = new BibleObjGituser(BibleObjJsonpApi.m_rootDir)
+            console.log(inp, "Proj_parse_usr_signin.start*************")
             var proj = userProject.Proj_parse_usr_signin(inp)
             if (!proj) return console.log(inp, "Proj_parse_usr_signin sign in failed.")
 
