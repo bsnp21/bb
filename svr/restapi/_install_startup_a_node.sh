3987e93efbb728cd99d2e2fdf0aa83e0289ce95a
wdingsoft <wdingbox@gmail.com>
2022-11-11 12:32:38 -0500

    try to install startup

---
+++ svr/restapi/_install_startup_a_node.sh
@@ -0,0 +1,6 @@
+
+
+
+# for ubuntu. 
+sudo ln -sf /var/www/html/wdaws/bb/svr/restapi/startup_a_node.sh /etc/rc6.d/startup_a_node.sh
+sudo chmod +x /etc/rc6.d/startup_a_node.sh
---
+++ svr/restapi/_startup_a_node.sh
@@ -0,0 +1,4 @@
+
+
+sudo node /var/www/html/wdaws/bb/svr/restapi/startup_a_node.sh
+
