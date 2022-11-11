f7ecfd31e578caaeeb0f95c758df1985577c4ec3
wdingsoft <wdingbox@gmail.com>
2022-11-11 12:59:00 -0500

    rc6.d

--- svr/restapi/_install_startup_a_node.sh
+++ svr/restapi/_install_startup_a_node.sh
@@ -6,3 +6,6 @@ sudo chmod 777 _startup_a_node.sh
 sudo rm /etc/rc6.d/_startup_a_node.sh
 sudo ln -sf /var/www/html/wdaws/bb/svr/restapi/_startup_a_node.sh /etc/rc6.d/_startup_a_node.sh
 sudo chmod 777 /etc/rc6.d/_startup_a_node.sh
+
+## check
+ls -l /etc/rc6.d/
