6e7b9c4ce6c3ddd176a0aecaf13a20d7e876bdfa
wdingsoft <wdingbox@gmail.com>
2022-11-11 12:54:45 -0500

    starup at /etc/rc6/d/starup_a_node

--- svr/restapi/_install_startup_a_node.sh
+++ svr/restapi/_install_startup_a_node.sh
@@ -2,5 +2,7 @@
 
 
 # for ubuntu. 
+sudo chmod 777 _startup_a_node.sh
+sudo rm /etc/rc6.d/startup_a_node.sh
 sudo ln -sf /var/www/html/wdaws/bb/svr/restapi/startup_a_node.sh /etc/rc6.d/startup_a_node.sh
-sudo chmod +x /etc/rc6.d/startup_a_node.sh
+sudo chmod 777 /etc/rc6.d/startup_a_node.sh
