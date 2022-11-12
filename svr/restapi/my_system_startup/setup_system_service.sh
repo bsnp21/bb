17fce947f620598b55c9cb1fd92f3c994a3e13c5
wdingsoft <wdingbox@gmail.com>
2022-11-12 07:57:43 -0500

    rename

--- svr/restapi/my_system_startup/setup_system_service.sh
+++ svr/restapi/my_system_startup/setup_system_service.sh
@@ -5,7 +5,7 @@
 ls -al /etc/systemd/system
 
 #sudo ln -sf /var/www/html/wdaws/bb/svr/restapi/_startup_a_node.sh /etc/systemd/system/_startup_a_node.sh
-sudo ln -sf /var/www/html/wdaws/bb/svr/restapi/myStartup/bsnp_a_node.service /etc/systemd/system/bsnp_a_node.service
+sudo ln -sf /var/www/html/wdaws/bb/svr/restapi/my_system_service/bsnp_a_node.service /etc/systemd/system/bsnp_a_node.service
 
 #sudo cp ./bsnp_a_node.service /etc/systemd/system/bsnp_a_node.service
 
