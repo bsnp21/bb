3f2e0d3879980a2a389def79d99975ec78fcc7ab
wdingsoft <wdingbox@gmail.com>
2022-11-12 15:31:51 -0500

    rename

--- svr/my_systemd_startup/setup_bsnpc_service.sh
+++ svr/my_systemd_startup/setup_bsnpc_service.sh
@@ -5,7 +5,7 @@
 ls -al /etc/systemd/system
 
 #sudo ln -sf /var/www/html/wdaws/bb/svr/restapi/_startup_a_node.sh /etc/systemd/system/_startup_a_node.sh
-sudo ln -sf /var/www/html/wdaws/bb/svr/restapi/my_system_startup/bsnpc.service /etc/systemd/system/bsnpc.service
+sudo ln -sf /var/www/html/wdaws/bb/svr/my_system_startup/bsnpc.service /etc/systemd/system/bsnpc.service
 
 #sudo cp ./bsnp_a_node.service /etc/systemd/system/bsnp_a_node.service
 
