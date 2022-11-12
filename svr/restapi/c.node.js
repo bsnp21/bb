9403f54efa63dea4da1d60b80564f9bfd9475935
wdingsoft <wdingbox@gmail.com>
2022-11-12 14:40:12 -0500

    -

--- svr/restapi/c.node.js
+++ svr/restapi/c.node.js
@@ -106,7 +106,7 @@ var MASTER_SVR = {
             str += "</tr>"
         })
         str += "</table><br>\r\n"
-        str += `<a href="https://${MASTER_SVR.get_ip()}:${MASTER_SVR.https}">https://:${MASTER_SVR.https}</a>`
+        str += `<a href="https://${MASTER_SVR.get_ip()}:${MASTER_SVR.https_port}">https://:${MASTER_SVR.https_port}</a>`
 
         return str
     },
