615042c2fa441e7dec9528c49241a482531a129a
wdingsoft <wdingbox@gmail.com>
2022-11-12 15:39:19 -0500

    rename

--- svr/my_systemd_startup/setup_bsnpa_service.sh
+++ svr/my_systemd_startup/setup_bsnpa_service.sh
@@ -14,4 +14,9 @@ sudo chmod 777 /etc/systemd/system/bsnpa.service
 
 ls -al /etc/systemd/system
 
+
+sudo systemctl enable bsnpa
+sudo systemctl start bsnpa
+sudo systemctl daemon-reload
+
 ps aux|grep a.node
