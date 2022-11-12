d6fcb3e9e037040263ea14239a7238399bf3891d
wdingsoft <wdingbox@gmail.com>
2022-11-12 15:16:36 -0500

    rename

--- svr/restapi/c.node.js
+++ svr/restapi/c.node.js
@@ -129,7 +129,7 @@ var MASTER_SVR = {
             cmd = req.query["exec"]
             ret = MASTER_SVR.exec_Cmd(cmd).then(
                 function (re) {
-                    re.stdout = re.stdout.replace(/(\n)/g, "<br><br>") + MASTER_SVR.ps_aux_grep_node(cmd, re.stdout)
+                    re.stdout = re.stdout.replace(/(\n)/g, "<br><hr/>") + MASTER_SVR.ps_aux_grep_node(cmd, re.stdout)
                     var str = `<pre>${dt}<br>${reqs}<br>${cmd}<br>${JSON.stringify(re, null, 4)}</pre>`
                     res.send(str);
                 },
