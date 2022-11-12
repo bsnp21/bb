9d03bb5658f1a1a6644e7c9e788a07a6704cb456
wdingsoft <wdingbox@gmail.com>
2022-11-12 09:26:26 -0500

    set bsnpc servic

---
+++ svr/restapi/my_system_startup/setup_bsnpc_service.sh
@@ -0,0 +1,17 @@
+
+#/bin
+
+
+ls -al /etc/systemd/system
+
+#sudo ln -sf /var/www/html/wdaws/bb/svr/restapi/_startup_a_node.sh /etc/systemd/system/_startup_a_node.sh
+sudo ln -sf /var/www/html/wdaws/bb/svr/restapi/my_system_startup/bsnpa.service /etc/systemd/system/bsnpa.service
+
+#sudo cp ./bsnp_a_node.service /etc/systemd/system/bsnp_a_node.service
+
+sudo chmod 777 /etc/systemd/system/bsnpa.service
+
+
+ls -al /etc/systemd/system
+
+ps aux|grep a.node
