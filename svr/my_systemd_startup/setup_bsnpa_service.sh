b581ed14419c325923e47e8a3dbfe3799052e199
wdingsoft <wdingbox@gmail.com>
2022-11-12 16:08:29 -0500

    fix systemd

--- svr/my_systemd_startup/setup_bsnpa_service.sh
+++ svr/my_systemd_startup/setup_bsnpa_service.sh
@@ -5,7 +5,7 @@
 ls -al /etc/systemd/system
 
 #sudo ln -sf /var/www/html/wdaws/bb/svr/restapi/_startup_a_node.sh /etc/systemd/system/_startup_a_node.sh
-sudo ln -sf /var/www/html/wdaws/bb/svr/my_system_startup/bsnpa.service /etc/systemd/system/bsnpa.service
+sudo ln -sf /var/www/html/wdaws/bb/svr/my_systemd_startup/bsnpa.service /etc/systemd/system/bsnpa.service
 
 #sudo cp ./bsnp_a_node.service /etc/systemd/system/bsnp_a_node.service
 
