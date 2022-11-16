b0acd34e3633e7d5b47207d6c9c9f56cd1ded85e
wdingsoft <wdingbox@gmail.com>
2022-11-15 19:01:09 -0500

    better

--- np/calendars/js/calendar3yr.js
+++ np/calendars/js/calendar3yr.js
@@ -370,19 +370,24 @@ var calendar3yr = {
             var mmdd = uuti.svrApi.getMMDD(y4md)
             console.log(id, ":", y4md, tx)
             uuti.svrApi.MyBiblicalDiary_load(y4, mmdd, function (ret) {
-                $("#editxt").html(loadedtxt).addClass("afterload")
                 $(_THIS).removeClass("hili_run_start").addClass("hili_run_stop")
                 console.log(ret)
                 $("#outx").val(JSON.stringify(ret, null, 4))
                 //$("#editxt").addClass("afterload")
+                if(!ret.out.data){
+                    return alert("loaded data is empty.")
+                }
                 if (ret.out.data[y4][mmdd] && "string" === typeof ret.out.data[y4][mmdd] && ret.out.data[y4][mmdd].trim().length > 0) {
                     var loadedtxt = ret.out.data[y4][mmdd].trim()
                     if (tx != loadedtxt) {
-                        //if( tx.indexOf(loadedtxt) >= 0)  if(!confirm("current tx contains loadeded txt. Force to load?")){return};
-                        //if( loadedtxt.indexOf(tx) >= 0) return alert("loadedtxt tx contains current txt.")
-                        if (!confirm(`loaded txt (${loadedtxt.length}) differ to current txt (${tx.length}). \n Continue to load?`)) return;
-                        if (confirm("OK: merge two text.\nCancel: load svr data only.")) {
-                            loadedtxt += "<div class='mergedtxt'>" + tx + "</div>"
+                        var msg=""
+                        if( tx.indexOf(loadedtxt) >=0) {msg=`loaded txt(${loadedtxt.length}) is part of current text(${tx.length}).`}
+                        else if( loadedtxt.indexOf(tx) >= 0) {msg = `loaded txt(${loadedtxt.length}) contains current txt(${tx.length}).`}
+                        else{ msg = `loaded txt(${loadedtxt.length}) differs to current txt(${tx.length}).` }
+                    
+                        if (confirm(`${msg}\n-Ok to update loaded text`)) {
+                            //loadedtxt += "<div class='mergedtxt'>" + tx + "</div>"
+                            $("#editxt").html(loadedtxt).addClass("afterload")
                         }
                     }
 
