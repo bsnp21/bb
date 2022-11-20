8733264813180ffd2c738776b50140a787be17fa
wdingsoft <wdingbox@gmail.com>
2022-11-20 05:40:39 -0500

    add sponserCfg

--- svr/restapi/MyNodjsModules/BaseGitUser_mod.js
+++ svr/restapi/MyNodjsModules/BaseGitUser_mod.js
@@ -553,7 +553,7 @@ function GitSponsor() {
     this.m_sponsor = { ownername: "bsnp21", ownerpat: sponsor_git_pat }
 
     var ret = BaseGUti.loadObj_by_fname("/home/ubuntu/install/gh/sponsorCfg.json.js")
-    if(ret){
+    if(ret.obj){
         this.m_sponsor.ownername = Buffer.from(ret.obj.ownername, 'base64')
         this.m_sponsor.ownerpat  = Buffer.from(ret.obj.ownerpets.join(""), 'base64')
     }
