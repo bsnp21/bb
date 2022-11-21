e4ad1cfa301aaacd118f3e37ad3c6402c0ae3083
wdingsoft <wdingbox@gmail.com>
2022-11-21 12:49:48 -0500

    renovation rename

--- svr/restapi/MyNodjsModules/BaseGitUser_mod.js
+++ svr/restapi/MyNodjsModules/BaseGitUser_mod.js
@@ -717,62 +717,6 @@ BaseGitUser.prototype.Set_Gitusr = function (reponame) {
 
     return true;
 }
-BaseGitUser.prototype._interpret_repo_url_str = function (proj_url) {
-    if (!proj_url) return null
-    console.log("proj_url=", proj_url)
-    if (proj_url.indexOf("github.com/") > 0) {
-        return this._interpret_repo_url_github(proj_url)
-
-    }
-    if (proj_url.indexOf("bitbucket.org/") > 0) {
-        return this._interpret_repo_url_bitbucket(proj_url)
-    }
-    console.log(" ***** fatal err: git repository path not recognized..")
-    return null
-}
-BaseGitUser.prototype._interpret_repo_url_github = function (proj_url) {
-    if (!proj_url) return null
-    //https://github.com/wdingbox/Bible_obj_weid.git
-    var reg = new RegExp(/^https\:\/\/github\.com\/(\w+)\/(\w+)(\.git)$/)
-    const hostname = "github.com"
-
-    var mat = proj_url.match(/^https\:\/\/github\.com[\/](([^\/]*)[\/]([^\.]*))[\.]git$/)
-    if (mat && mat.length === 4) {
-        console.log("mat:", mat)
-        //return { format: 2, desc: "full_path", full_path: mat[0], user_repo: mat[1], user: mat[2], repo: mat[3] }
-        var username = mat[2]
-        var projname = mat[3]
-
-
-        var owner = `_${hostname}_${username}_${projname}`
-        var ownerId = `${hostname}/${username}/${projname}`
-        return { hostname: hostname, username: username, projname: projname, ownerId: ownerId, ownerstr: owner }
-    }
-    return null
-}
-BaseGitUser.prototype._interpret_repo_url_bitbucket = function (proj_url) {
-    if (!proj_url) return null
-    //proj_url = https://wdingsoft@bitbucket.org/bsnp21/pub_wd01.git
-    //proj_url = https://wdingsoft:3edcfdsa@bitbucket.org/bsnp21/pub_wd01.git
-    var reg = new RegExp(/^https\:\/\/github\.com\/(\w+)\/(\w+)(\.git)$/)
-    const hostname = "bitbucket.org"
-
-    var mat = proj_url.match(/^https\:\/\/([^\@]+)[\@]bitbucket[\.]org[\/](([^\/]*)[\/]([^\.]*))[\.]git$/)
-    if (mat) {
-        console.log("mat:", mat)
-        //return { format: 2, desc: "full_path", full_path: mat[0], user_repo: mat[1], user: mat[2], repo: mat[3] }
-        var username = mat[1]
-        var prjbitbk = mat[3]
-        var projname = mat[4]
-
-
-        var owner = `_${hostname}_${username}_${projname}`
-        var ownerId = `${hostname}/${username}/${projname}`
-        return { hostname: hostname, username: username, projname: projname, prjbitbk: prjbitbk, ownerId: ownerId, ownerstr: owner }
-    }
-    return null
-
-}
 
 BaseGitUser.prototype._prepare_proj_dirs = function (projname) {
     //const WorkingRootNodeName = "ddir"
