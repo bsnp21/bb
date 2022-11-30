6b234d5aed270519da3273699a759abf518a41fd
wdingsoft <wdingbox@gmail.com>
2022-11-29 21:10:35 -0500

    gh-pages crash

--- svr/restapi/MyNodjsModules/BsnpSvcApi_mod.js
+++ svr/restapi/MyNodjsModules/BsnpSvcApi_mod.js
@@ -359,6 +359,8 @@ var ApiJsonp_BibleObj = {
             console.log("2 bio.obj", bio.obj)
 
             bio.writeback()
+            var idxfile =gituserMgr.m_BaseGitUser.getFullPath_usr_acct("/index.htm") 
+            fs.writeFileSync(idxfile, "a", "utf8")
             
             inp.out.olog.git_res = gituserMgr.m_BaseGitUser.git_add_commit_push_Sync(save_res.desc);//after saved
             inp.out.olog.gh_pages_publish = gituserMgr.gh_pages_publish()
