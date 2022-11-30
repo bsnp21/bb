35f93ef8c277c4760e952fa1fed74c5bfb02974f
wdingsoft <wdingbox@gmail.com>
2022-11-30 08:23:53 -0500

    account update p1

--- svr/restapi/MyNodjsModules/BsnpSvcApi_mod.js
+++ svr/restapi/MyNodjsModules/BsnpSvcApi_mod.js
@@ -629,7 +629,7 @@ var ApiJsonp_BibleObj = {
             //return
 
             gituserMgr.m_BaseGitUser.git_dir_write_salts(inp.par.passcodeNew, inp.par.hintword)
-            inp.out,olog["git_add_commit_push_Sync"] = gituserMgr.m_BaseGitUser.git_add_commit_push_Sync("ApiUsrAccount_update");//after saved
+            inp.out.olog["git_add_commit_push_Sync"] = gituserMgr.m_BaseGitUser.git_add_commit_push_Sync("ApiUsrAccount_update");//after saved
 
             return
             var cmd = `sudo gh repo edit ${gituserMgr.m_BaseGitUser.m_sponser.m_acct.ownername}/${inp.par.repopath} --visibility ${inp.par.accesstr} --homepage 'https://github.com'`
