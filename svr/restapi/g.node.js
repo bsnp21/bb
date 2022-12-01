380c10fa0ce1233767b88962457f58379ee68f6e
wdingsoft <wdingbox@gmail.com>
2022-12-01 05:26:40 -0500

    gh-pages test

--- svr/restapi/g.node.js
+++ svr/restapi/g.node.js
@@ -147,7 +147,7 @@ function gh_pages_test_muplitple_dest_by_argv2() {
     var envKey = `GH_TOKEN_${ownername}`
     var envs = process.env[envKey]
     console.log("process.env", process.env)
-    console.log("env", envs)
+    //console.log("env", envs)
     console.log("envKey", envKey)
 
 
