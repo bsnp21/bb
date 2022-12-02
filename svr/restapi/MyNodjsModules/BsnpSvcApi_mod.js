fe7041f59ce7286bb81da7a31fa6a87426392cb8
wdingsoft <wdingbox@gmail.com>
2022-12-02 17:14:58 -0500

    prepare private

--- svr/restapi/MyNodjsModules/BsnpSvcApi_mod.js
+++ svr/restapi/MyNodjsModules/BsnpSvcApi_mod.js
@@ -407,7 +407,10 @@ var ApiJsonp_BibleObj = {
                     TargNodeNotOwnProperty: function (carProperty, carObj, targObj, tarParent, tarParentProperty) {//at the end of object tree.
                         //targObj[carProperty] = carObj[carProperty] //at the end of object tree, make a copy or src.
                         bUpdatedUsersList = true
-                        tarParent[tarParentProperty] = carObj
+                        if("object" !== typeof(targObj)){
+                            tarParent[tarParentProperty] = {}
+                        }
+                        tarParent[tarParentProperty][carProperty] = carObj[carProperty]
                         ret.TargNodeNotOwnProperty = [carProperty, carObj, targObj, tarParent, tarParentProperty, ret.admobj.obj]
                     }
                 })
