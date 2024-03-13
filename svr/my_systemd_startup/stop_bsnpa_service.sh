03128865398f16786401ae0b85506a6ab028786e
wdingsoft <wdingbox@gmail.com>
2024-03-13 19:52:17 -0400

    comments remark

--- svr/my_systemd_startup/stop_bsnpa_service.sh
+++ svr/my_systemd_startup/stop_bsnpa_service.sh
@@ -11,6 +11,9 @@ sudo systemctl stop bsnpa
 sudo systemctl disable bsnpa
 sudo systemctl daemon-reload
 
-journalctl -u bsnpa
 
+journalctl -u bsnpa -f
+
+
+echo "stop a.node.js systemd service"
 ps aux|grep a.node
