f37c23f6ac69b26cde2151b4fc7efb1d5d7b7d95
wdingsoft <wdingbox@gmail.com>
2024-06-29 11:58:09 -0400

    salts issue

--- svr/restapi/MyNodjsModules/BaseGitUser_mod.js
+++ svr/restapi/MyNodjsModules/BaseGitUser_mod.js
@@ -1245,7 +1245,7 @@ BaseGitUser.prototype.main_dir_write_salts = function (passcode, hintword) {
 
     var fname = this.getFullPath_usr_main("index.htm")
     //var ret = fs.writeFileSync(fname, "making salts", function (er) {
-        console.log("write ret", er)
+        //console.log("write ret", er)
     //})
 
     console.log("write salts: ", fname, salts)
